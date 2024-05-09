11ac88cf785bb842e79b44facb0eacb5bbf9e7d6
Parthib Nandy <parthibnandy1234@gmail.com>
2024-05-09 17:43:27 +0530

    all setup configured

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv"
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectDB()
