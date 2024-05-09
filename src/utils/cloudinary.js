3d7ee614bbd195b114046083a0038d5758170b9a
Parthib Nandy <parthibnandy1234@gmail.com>
2024-05-09 17:14:19 +0530

    multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+//after getting local file path of the file upload it on cloudinary
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";//managing file system 
+
+
+
+    // Configuration
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET  
+});
+
+const uploadOnCloudinary= async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null
+        }
+
+            //upload file on cloudinary
+            const response = await cloudinary.uploader.upload(localFilePath,{
+                resource_type:"auto"
+            })
+            console.log("File is Uploaded on Cloudinary ",response.url); 
+            return response; 
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//to remove the locally saved temp file if not uploaded
+        return null;
+    }
+}
+    
+
+export {uploadOnCloudinary}
