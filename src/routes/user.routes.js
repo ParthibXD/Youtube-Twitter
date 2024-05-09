83231ed5b471b5685568bfe3d3421ad0610444f8
Parthib Nandy <parthibnandy1234@gmail.com>
2024-05-09 23:30:13 +0530

    logout and login

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 
@@ -20,5 +20,8 @@ router.route("/register").post(
     registerUser
 )
 
+router.route("/login").post(loginUser)
 
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 export default router
