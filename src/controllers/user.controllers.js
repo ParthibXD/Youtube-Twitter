63b9d23f1e8550c42e56e7be329327d122d36345
Parthib Nandy <parthibnandy1234@gmail.com>
2024-05-11 17:19:25 +0530

    channel controller created

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -384,6 +384,8 @@ const updateUserAvatar = asyncHandler( async (req, res) => {
         throw new ApiError(400, "Error while uploading on avatar")
     }
 
+    //TODO: delete old image
+
     const user = await User.findOneAndUpdate(
         req.user?._id,
         {
@@ -442,6 +444,83 @@ const updateUserCoverImage = asyncHandler( async (req, res) => {
             "Cover Image updated Successfully"
         )
     )
+})
+
+const getUserChannelProfile = asyncHandler( async (req,res) =>{
+    const{ username }=req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is  Missing");
+    }
+
+    //User.find({username})
+
+    const channel = await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"//count subscriber
+                },
+                channelSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false,
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,//1 means selected o
+                username:1,
+                subscribersCount:1,
+                channelSubscribedToCount:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+                
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exists")
+    }
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channel[0],
+            "User channel fetched successfully"
+        )
+    )
+
 })
 
 export {
